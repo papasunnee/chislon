5f290b7645c94c97333a1329cf0803faa6b56386
Adetona Sunday <papasunnee@gmail.com>
2019-08-28 17:47:35 +0100

    uploadSideBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { slide as Menu } from "react-burger-menu";
 import Link from "next/link";
+import "./index.css";
 import {
   Collapse,
   Navbar,
@@ -26,30 +28,40 @@ const Index = () => {
 
   return (
     <div>
-      <Navbar color="dark" dark expand="md">
+      <nav className="navbar navbar-default bg-dark">
         <NavbarBrand href="/">
           <img src="/static/images/whitelogo.png" className="img-fluid" />
         </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            {Menus.map(({ href, label }, index) => {
-              return (
-                <NavItem key={index}>
-                  <Link href={href} passHref>
-                    <NavLink>{label}</NavLink>
-                  </Link>
-                </NavItem>
-              );
-            })}
-            <NavItem className="">
-              <Link href="/login" passHref>
-                <NavLink style={{ color: "#FFDA42" }}>Login</NavLink>
-              </Link>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+        {/* <NavbarToggler onClick={toggle} /> */}
+
+        {/* <Collapse isOpen={isOpen} navbar> */}
+        {/* <Nav className="ml-auto" navbar>
+        
+          <NavItem className="">
+            <Link href="/login" passHref>
+              <NavLink style={{ color: "#FFDA42" }}>Login</NavLink>
+            </Link>
+          </NavItem>
+        </Nav> */}
+        <Menu right>
+          {Menus.map(({ href, label }, index) => {
+            return (
+              <NavItem key={index}>
+                <Link href={href} passHref>
+                  <NavLink>{label}</NavLink>
+                </Link>
+              </NavItem>
+            );
+          })}
+
+          <NavItem className="login">
+            <Link href="/login" passHref>
+              <NavLink style={{ color: "#FFDA42 !important" }}>Login</NavLink>
+            </Link>
+          </NavItem>
+        </Menu>
+        {/* </Collapse> */}
+      </nav>
     </div>
   );
 };
