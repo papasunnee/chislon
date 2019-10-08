2fba8a03ba421a9dae5968b9950aa91c4e9d0c44
Adetona Sunday <papasunnee@gmail.com>
2019-10-08 17:25:55 +0100

    cxxcxv

--- src/components/Common/Logos.js
+++ src/components/Common/Logos.js
@@ -24,14 +24,46 @@ const Logos = () => {
         </div>
 
         <div className="col-md-12 my-4">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus natus
-            quaerat similique perspiciatis mollitia ab explicabo minima placeat.
-            Autem impedit tenetur harum ut itaque a delectus atque aut
-            laudantium excepturi.
-          </p>
+          <div className="logos">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(
+              (img, index) => {
+                return (
+                  <div
+                    style={{
+                      position: "relative",
+                      display: "flex",
+                      alignItems: "center"
+                    }}
+                  >
+                    <img
+                      src={`static/images/partners/${img}.png`}
+                      key={index}
+                      className="img-fluid"
+                    />
+                  </div>
+                );
+              }
+            )}
+          </div>
         </div>
       </div>
+      <style jsx>{`
+        .logos {
+          display: flex;
+          justify-content: center;
+          flex-wrap: wrap;
+          max-width: 800px;
+          margin: auto;
+        }
+        .logos img {
+          width: auto;
+          margin: 10px;
+          transition: filter 0.5s cubic-bezier(0.17, 0.67, 0.93, 0.28);
+        }
+        .logos img:hover {
+          filter: grayscale(70%);
+        }
+      `}</style>
     </div>
   );
 };
