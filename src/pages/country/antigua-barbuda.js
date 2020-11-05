1b9347ef085a33e875705c9def9b6f86963fb919
Adetona Sunday <papasunnee@gmail.com>
2020-11-05 16:25:13 +0100

    update barbuda

--- src/pages/country/antigua-barbuda.js
+++ src/pages/country/antigua-barbuda.js
@@ -1,11 +1,198 @@
 import React, { Fragment } from "react";
-import { Container, Jumbotron } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import { CountryJumbo } from "../../components/Countries/index";
 
 export default () => {
   return (
     <Fragment>
       <CountryJumbo title="ANTIGUA AND BARBUDA" />
+      <Container>
+        <Row>
+          <Col sm={12} md={8}>
+            <p className="text-center">
+              The largest of the English-speaking Leeward Islands attracts
+              visitors with its mesmerizing beaches of the finest white sand and
+              azure waters. Join the celebrities who have chosen the island as a
+              place for winter homes and neighbor with Oprah Winfrey, Eric
+              Clapton, and many more.
+            </p>
+
+            <h5>Why Choose ANTIGUA & BARBUDA</h5>
+            <p>
+              The combined size of 442 square kilometers and a population of
+              over 80,000 make Antigua & Barbuda the ninth-largest country by
+              size in the Caribbean. The country’s economic citizenship program
+              facilitates the comfort of visa-free travel to more than 100
+              countries.
+            </p>
+            <p className="font-weight-bold">
+              Antigua & Barbuda recognizes dual citizenship, which can prove
+              advantageous for business expansion and tax relief. Other benefits
+              include the following:
+            </p>
+            <ul>
+              <li>Application processing within three to four months.</li>
+              <li>
+                Inclusion of dependent children under 28 and dependent parents
+                or grandparents over 58.
+              </li>
+              <li>
+                No requirement to travel to Antigua & Barbuda during the
+                application process.
+              </li>
+              <li>
+                No interview, education or managerial experience required.
+              </li>
+              <li>
+                Visa-free travel to more than 100 countries, including the
+                Schengen zone, the U.K., Hong Kong, Singapore and more.
+              </li>
+              <li>No tax on worldwide income.</li>
+            </ul>
+            <h5>
+              ANTIGUA AND BARBUDA CITIZENSHIP BY INVESTMENT QUALIFICATIONS
+            </h5>
+            <p>
+              Antigua & Barbuda’s Citizenship by Investment Program was
+              established in 2012. To qualify for citizenship applicants must
+              fulfill one of the investment options below in addition to meeting
+              the following criteria:
+            </p>
+
+            <ul>
+              <li>Be of outstanding character.</li>
+              <li>Hold no criminal record.</li>
+              <li>Have excellent health.</li>
+              <li>Have a high personal net worth.</li>
+              <li>
+                Reside with the family members who are included in the
+                application in the country for a minimum of 5 days within a
+                period of five years.
+              </li>
+            </ul>
+            <p>
+              Individuals born in countries on Antigua & Barbuda’s restricted
+              countries’ list (Afghanistan, Iran, Iraq, North Korea,
+              Somalia,Yemen, Sudan) but who have migrated before the age of
+              majority and/or have maintained permanent residence in Canada, The
+              United Kingdom, The United States of America, Australia, New
+              Zealand, Saudi Arabia and The United Arab Emirates for a period of
+              not less than 10 years and maintain no economic ties to any
+              restricted country may qualify for application.
+            </p>
+            <h5>ANTIGUA AND BARBUDA INVESTMENT OPTIONS</h5>
+            <p>
+              The government adopted the citizenship by investment program as
+              one of the mechanisms to promote economic growth, attract
+              developments, increase foreign direct investment to the country,
+              support the development of infrastructure and provide for a
+              sustainable future. As a result, investors can choose from several
+              options to qualify for the program.
+            </p>
+            <ol>
+              <li>
+                <span className="font-weight-bold">
+                  National Development Fund Contribution or Charitable Donation
+                </span>
+                <p>
+                  High net worth individuals may choose to make a monetary
+                  contribution of US$100,000 to the National Development Fund or
+                  an approved charity.
+                </p>
+              </li>
+              <li>
+                <span className="font-weight-bold">Real Estate Investment</span>
+                <p>
+                  Applicants may purchase property valued at a minimum of
+                  US$400,000 in a preapproved real estate development area. The
+                  investment must be maintained for a minimum of five years.
+                  Property registration, processing fees and taxes may be paid
+                  in addition to the property purchase. Until 31st October 2020,
+                  there is a Limited Time Offer (LTO); Two (2) applications from
+                  related parties can make a joint investment, with each
+                  applicant investing a minimum of US $200,000 in order to
+                  qualify.
+                </p>
+              </li>
+              <li type="none">
+                <span className="font-weight-bold">
+                  Callaloo Cay (Waldorf Astoria Antigua)
+                </span>
+                <p>
+                  Callaloo Cay has signed a management agreement with Waldorf
+                  Astoria Hotels & Resorts, Hilton’s luxury brand of iconic
+                  landmark hotels for the brand’s first new-build resort in the
+                  Caribbean, Waldorf Astoria Antigua.
+                </p>
+                <p>
+                  The resort’s 95 guest rooms and 25 branded villas, including
+                  beachfront suites and hilltop accommodations, will boast
+                  breathtaking views of the bay and calming lush surroundings,
+                  providing the ideal space for a comfortable and exclusive
+                  stay. This exclusive resort sits on 36 acres of prime
+                  beach-front and offers first-class facilities that cater to
+                  discerning clients.
+                </p>
+              </li>
+              <li type="none">
+                <span className="font-weight-bold">
+                  Half Moon Bay (Rosewood)
+                </span>
+                <p>
+                  Half Moon Bay Antigua is the first resort and residential
+                  community of its kind in the Caribbean. An intimate new
+                  enclave on the island of Antigua, Half Moon Bay will redefine
+                  ultra-luxury in modern terms that are meaningful to today’s
+                  discerning vacation homebuyer.
+                </p>
+                <p>
+                  Situated on 132 acres, the exclusive community is comprised of
+                  10 premier estate parcels that will be joined by a collection
+                  of Rosewood Residences and the new 47-suite Rosewood Half Moon
+                  Bay Hotel. Architecture for the Rosewood Hotel at Half Moon
+                  Bay is a collaboration between OBMI, the Caribbean’s
+                  preeminent architects, and Studio Piet Boon, an
+                  Amsterdam-based design firm with a high profile in the
+                  Caribbean ultra-luxury space. The vision is inspired by
+                  layering the sensibilities of traditional Caribbean
+                  architecture with a modern and distinctly environmental
+                  aesthetic.
+                </p>
+                <p>
+                  Additional properties and financing options are available upon
+                  request.
+                </p>
+              </li>
+            </ol>
+            <ol start="3">
+              <li>
+                <span className="font-weight-bold">Business Establishment</span>
+                <p>
+                  Applicants may invest US$1.5 million to establish a business.
+                  Two or more applicants may make a joint business investment
+                  with an individual threshold of at least US$400,000 and a
+                  total investment of at least US$5 million.
+                </p>
+              </li>
+              <li>
+                <span className="font-weight-bold">
+                  University of West Indies Fund
+                </span>
+                <p>
+                  For an investment of US$150,000, a family of six (6) is
+                  eligible to apply for citizenship. The investment amount
+                  includes processing and government fees, however customary due
+                  diligence fees do apply for each dependent. Dependents can
+                  include children below 30 years old and dependent parents over
+                  58 years old. In addition, one member of the family is
+                  entitled to a 1-year scholarship at the University of West
+                  Indies.
+                </p>
+              </li>
+            </ol>
+          </Col>
+        </Row>
+      </Container>
     </Fragment>
   );
 };
