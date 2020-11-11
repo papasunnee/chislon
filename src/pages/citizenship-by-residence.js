b9f28ed0ac2de8392fd7fcef22e5cd3577b6de50
Adetona Sunday <papasunnee@gmail.com>
2020-11-11 12:27:03 +0100

    update

--- src/pages/citizenship-by-residence.js
+++ src/pages/citizenship-by-residence.js
@@ -5,11 +5,11 @@ import { CountryJumbo } from "../components/Countries/index";
 export default () => {
   return (
     <Fragment>
-      <CountryJumbo title="CITIZENSHIP BY INVESTMENT" />
+      <CountryJumbo title="CITIZENSHIP BY RESIDENCE" />
       <Container>
         <Row>
           <Col md={12}>
-            <h5>What is Citizenship By Investment</h5>
+            <h5>What is Citizenship By Residence</h5>
           </Col>
         </Row>
       </Container>
