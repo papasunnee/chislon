4bbe2b1a3f9aad0a9819545b9a2d40f6fd55c06b
Adetona Sunday <papasunnee@gmail.com>
2020-11-16 00:13:19 +0100

    update

--- src/pages/healthcare-abroad.js
+++ src/pages/healthcare-abroad.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import Link from "next/link";
 import { Col, Container, Row } from "reactstrap";
 import { CountryJumbo } from "../components/Countries/index";
 
@@ -9,10 +10,64 @@ export default () => {
       <Container>
         <Row>
           <Col md={12}>
-            <h5>HEALTHCARE ABROAD</h5>
+            <h3>GREAT OPPORTUNITIES WITHIN SHORTAGE OCCUPATIONS</h3>
+          </Col>
+
+          <Col md={{ size: 5 }}>
+            <CountryBox country="U.S.A" href="/usa" />
+          </Col>
+          <Col md={2}></Col>
+          <Col md={{ size: 5 }}>
+            <CountryBox country="UK" href="/united-kingdom" />
+          </Col>
+          <Col md={{ size: 5 }}>
+            <CountryBox country="CANADA" href="quebec-canada" />
+          </Col>
+          <Col md={2}></Col>
+          <Col md={{ size: 5 }}>
+            <CountryBox country="AUSTRALIA" href="/australia" />
+          </Col>
+          <Col md={{ size: 5 }}>
+            <CountryBox country="CANADA" href="quebec-canada" />
+          </Col>
+          <Col md={2}></Col>
+          <Col md={{ size: 5 }}>
+            <CountryBox country="AUSTRALIA" href="/australia" />
           </Col>
         </Row>
       </Container>
+    </Fragment>
+  );
+};
+
+export const CountryBox = ({ country = "USA", href = "" }) => {
+  return (
+    <Fragment>
+      <Link href={`/country${href}`}>
+        <a className="country">
+          <span>{country}</span>
+        </a>
+      </Link>
+      <style jsx>
+        {`
+          .country {
+            border: 1px solid #b2b2b2;
+            background-color: #ebebeb;
+            height: 350px;
+            margin: 50px 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 2.5rem;
+            font-weight: bold;
+            transition: all 0.5s ease-in;
+            text-decoration: none;
+          }
+          .country:hover {
+            background-color: #fcfcfc;
+          }
+        `}
+      </style>
     </Fragment>
   );
 };
