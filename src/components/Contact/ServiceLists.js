b8d5a2d5a19f292271ff06622bbd2e627497f2df
Adetona Sunday <papasunnee@gmail.com>
2020-11-29 23:29:15 +0100

    contact update

--- src/components/Contact/ServiceLists.js
+++ src/components/Contact/ServiceLists.js
@@ -1,10 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Col } from "reactstrap";
+import axios from "axios";
+import dynamic from "next/dynamic";
+import { Loading, encode } from "../../util";
 import SectionStyle from "../styles/SectionStyle";
 import Heading3Style from "../styles/Heading3";
 import ButtonStyle from "../styles/ButtonStyle";
 
+const SweetAlert = dynamic(
+  () => {
+    return import("sweetalert2-react");
+  },
+  { ssr: false }
+);
+
+const initialValues = {
+  salutation: "Mr",
+  firstname: "",
+  lastname: "",
+  media: "",
+  position: "",
+  nationality: "Afghanistan",
+  country_of_residence: "Afghanistan",
+  website: "",
+  countries: [],
+  phone: "",
+  email: "",
+  message: "",
+};
 const Study = () => {
+  const [form, setForm] = useState(initialValues);
+  const [alertState, setAlertState] = useState(false);
+  const [btnState, setBtnState] = useState(false);
+  const getCountries = async () => {
+    const result = await axios("https://restcountries.eu/rest/v2/all");
+    setForm((prevValues) => ({
+      ...prevValues,
+      countries: [...result.data],
+    }));
+  };
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  const handleSubmit = (event) => {
+    setBtnState(true);
+    event.preventDefault();
+    fetch("", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": event.target.getAttribute("name"),
+        ...form,
+      }),
+    })
+      .then(() => {
+        setBtnState(false);
+        setForm(initialValues);
+        setAlertState(true);
+      })
+      .catch((error) => {
+        alert("Please, try submitting your data again");
+        setBtnState(false);
+        // setForm(initialValues);
+        console.log(error);
+      });
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
   return (
     <SectionStyle className="serviceSectionStyle">
       <Row>
@@ -12,12 +80,30 @@ const Study = () => {
           <div className="formWrapper">
             <Heading3Style>Enquiry Form</Heading3Style>
             <p>Send us a message</p>
-            <form>
+            <form
+              name="contact_form"
+              method="POST"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              onSubmit={handleSubmit}
+            >
+              <SweetAlert
+                show={alertState}
+                title="Contact Message"
+                text="Your Message Has been Successfully Submitted"
+                onConfirm={() => setAlertState(false)}
+              />
               <Row>
                 <Col sm={12} md={1}>
                   <div className="form-group">
                     <label>Salutation</label>
-                    <select name="" id="" className="form-control">
+                    <select
+                      name="salutation"
+                      id="salutation"
+                      className="form-control"
+                      value={form.salutation}
+                      onChange={handleChange}
+                    >
                       <option value="Mr">Mr</option>
                       <option value="Mrs">Mrs</option>
                       <option value="Miss">Miss</option>
@@ -29,61 +115,158 @@ const Study = () => {
                 <Col sm={12} md={5}>
                   <div className="form-group">
                     <label>First Name</label>
-                    <input type="text" className="form-control" required />
+                    <input
+                      type="text"
+                      name="firstname"
+                      id="firstname"
+                      className="form-control"
+                      value={form.firstname}
+                      onChange={handleChange}
+                      required
+                    />
                   </div>
                 </Col>
                 <Col sm={12} md={5}>
                   <div className="form-group">
                     <label>Last Name</label>
-                    <input type="text" className="form-control" required />
+                    <input
+                      type="text"
+                      name="lastname"
+                      id="lastname"
+                      className="form-control"
+                      value={form.lastname}
+                      onChange={handleChange}
+                      required
+                    />
                   </div>
                 </Col>
                 <Col sm={12} md={6}>
                   <div className="form-group">
                     <label>Media Organization</label>
-                    <input type="text" className="form-control" required />
+                    <input
+                      type="text"
+                      name="media"
+                      id="media"
+                      className="form-control"
+                      value={form.media}
+                      onChange={handleChange}
+                      required
+                    />
                   </div>
                 </Col>
                 <Col sm={12} md={5}>
                   <div className="form-group">
                     <label>Position</label>
-                    <input type="text" className="form-control" required />
+                    <input
+                      type="text"
+                      name="position"
+                      id="position"
+                      className="form-control"
+                      value={form.position}
+                      onChange={handleChange}
+                      required
+                    />
                   </div>
                 </Col>
                 <Col sm={12} md={6}>
                   <div className="form-group">
-                    <label>Nationality</label>
-                    <input type="text" className="form-control" required />
+                    <label htmlFor="">
+                      NATIONALITY <span className="text-danger">*</span>
+                    </label>
+                    <select
+                      name="nationality"
+                      id="nationality"
+                      value={form.nationality}
+                      onChange={handleChange}
+                      className="form-control"
+                    >
+                      {form.countries.map((country, index) => {
+                        return (
+                          <option key={index} value={country.name}>
+                            {country.name}
+                          </option>
+                        );
+                      })}
+                    </select>
                   </div>
                 </Col>
                 <Col sm={12} md={5}>
                   <div className="form-group">
-                    <label>Country of Residence</label>
-                    <input type="text" className="form-control" required />
+                    <label htmlFor="">
+                      COUNTRY OF RESIDENCE{" "}
+                      <span className="text-danger">*</span>
+                    </label>
+                    <select
+                      name="country_of_residence"
+                      id="country_of_residence"
+                      value={form.country_of_residence}
+                      onChange={handleChange}
+                      className="form-control"
+                    >
+                      {form.countries.map((country, index) => {
+                        return (
+                          <option key={index} value={country.name}>
+                            {country.name}
+                          </option>
+                        );
+                      })}
+                    </select>
                   </div>
                 </Col>
                 <Col sm={12} md={6}>
                   <div className="form-group">
                     <label>Website</label>
-                    <input type="text" className="form-control" required />
+                    <input
+                      type="url"
+                      name="website"
+                      id="website"
+                      className="form-control"
+                      value={form.website}
+                      onChange={handleChange}
+                      required
+                    />
                   </div>
                 </Col>
                 <Col sm={12} md={5}>
                   <div className="form-group">
                     <label>Phone Number</label>
-                    <input type="text" className="form-control" required />
+                    <input
+                      type="phone"
+                      name="phone"
+                      id="phone"
+                      className="form-control"
+                      value={form.phone}
+                      onChange={handleChange}
+                      required
+                    />
                   </div>
                 </Col>
                 <Col sm={12} md={6}>
                   <div className="form-group">
                     <label>Email</label>
-                    <input type="text" className="form-control" required />
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="form-control"
+                      value={form.email}
+                      onChange={handleChange}
+                      required
+                    />
                   </div>
                 </Col>
                 <Col sm={12} md={5}>
                   <div className="form-group">
-                    <label>Email (Repeat) </label>
-                    <input type="text" className="form-control" required />
+                    <label>Your Message</label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      className="form-control"
+                      value={form.message}
+                      onChange={handleChange}
+                      required
+                      rows="10"
+                    />
                   </div>
                 </Col>
                 <Col sm={12} md={6}>
@@ -95,15 +278,16 @@ const Study = () => {
                     </p>
                   </div>
                 </Col>
-                <Col sm={12} md={5}>
-                  <div className="form-group">
-                    <label>Your Message</label>
-                    <textarea className="form-control" rows="10" />
-                  </div>
-                </Col>
+
                 <Col md={12}>
                   <div className="form-group text-center">
-                    <ButtonStyle type="submit">SEND MESSAGE</ButtonStyle>
+                    <ButtonStyle
+                      type="submit"
+                      className="btn submitButton"
+                      disabled={btnState}
+                    >
+                      Send Message {btnState && <Loading />}
+                    </ButtonStyle>
                   </div>
                 </Col>
               </Row>
